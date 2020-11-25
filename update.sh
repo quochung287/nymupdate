46f276db30d69702ff68104aa6f4ea545f8637e5
botd0tnet <74009258+botd0tnet@users.noreply.github.com>
2020-11-25 14:03:02 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,15 +30,15 @@ if [ ! -f nym-mixnode_linux_x86_64 ] || [ "$(./nym-mixnode_linux_x86_64 --versio
    then
        if systemctl list-units --state=running | grep nym-mixnode
           then echo "stopping nym-mixnode.service to update the node ..." && systemctl stop nym-mixnode
-	  	sleep 3
-	  	sudo rm /home/nym/nym-mixnode_linux_x86_64
+	  	sudo mv nym-mixnode_linux_x86_64 nym-mixnode_linux_x86_64_0.9.0
+#	  	sudo rm /home/nym/nym-mixnode_linux_x86_64
 		sudo -u nym wget https://github.com/nymtech/nym/releases/download/v0.9.1/nym-mixnode_linux_x86_64
           else echo " nym-mixnode.service is inactive or not existing. Downloading new binaries ..."
 	  	sudo rm /home/nym/nym-mixnode_linux_x86_64
 		sudo -u nym wget https://github.com/nymtech/nym/releases/download/v0.9.1/nym-mixnode_linux_x86_64
 	   # Make it executable
    sudo -u nym chmod +x ./nym-mixnode_linux_x86_64 && chown nym:nym ./nym-mixnode_linux_x86_64
-   chmod +x ./nym-mixnode_linux_x86_64 && chown nym:nym ./nym-mixnode_linux_x86_64   
+#   chmod +x ./nym-mixnode_linux_x86_64 && chown nym:nym ./nym-mixnode_linux_x86_64   
    fi
 else
    echo "You have the latest version of Nym-mixnode $VERSION"
