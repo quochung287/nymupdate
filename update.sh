20e4545fe09c794234893ea01406677694d72c71
quochung287 <74443840+quochung287@users.noreply.github.com>
2020-11-27 09:53:56 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,12 +30,12 @@ if [ ! -f nym-mixnode_linux_x86_64 ] || [ "$(./nym-mixnode_linux_x86_64 --versio
    then
        if systemctl list-units --state=running | grep nym-mixnode
           then echo "stopping nym-mixnode.service to update the node ..." && systemctl stop nym-mixnode
-	  	sudo mv nym-mixnode_linux_x86_64 nym-mixnode_linux_x86_64_0.9.0
+	  	sudo mv nym-mixnode_linux_x86_64 nym-mixnode_linux_x86_64_0.9.1
 #	  	sudo rm /home/nym/nym-mixnode_linux_x86_64
-		sudo -u nym wget https://github.com/nymtech/nym/releases/download/v0.9.1/nym-mixnode_linux_x86_64
+		sudo -u nym wget https://github.com/nymtech/nym/releases/download/v0.9.2/nym-mixnode_linux_x86_64
           else echo " nym-mixnode.service is inactive or not existing. Downloading new binaries ..."
 	  	sudo rm /home/nym/nym-mixnode_linux_x86_64
-		sudo -u nym wget https://github.com/nymtech/nym/releases/download/v0.9.1/nym-mixnode_linux_x86_64
+		sudo -u nym wget https://github.com/nymtech/nym/releases/download/v0.9.2/nym-mixnode_linux_x86_64
 	  fi		
  # Make it executable
    sudo -u nym chmod +x ./nym-mixnode_linux_x86_64 && chown nym:nym ./nym-mixnode_linux_x86_64
@@ -49,15 +49,15 @@ fi
 function upgrade_nym () {
      #set -x
      sudo echo -n "" > /etc/systemd/system/nym-mixnode.service
-     directory='SF'
+     directory='NymMixNode'
 	
      #id=$(echo "$i" | rev | cut -d/ -f1 | rev)
      printf '%s\n' "[Unit]" > /etc/systemd/system/nym-mixnode.service
-     printf '%s\n' "Description=Nym Mixnode (0.9.1)" >> /etc/systemd/system/nym-mixnode.service
+     printf '%s\n' "Description=Nym Mixnode (0.9.2)" >> /etc/systemd/system/nym-mixnode.service
      printf '%s\n' "" >> /etc/systemd/system/nym-mixnode.service
      printf '%s\n' "[Service]" >> /etc/systemd/system/nym-mixnode.service
      printf '%s\n' "User=nym" >> /etc/systemd/system/nym-mixnode.service
-     printf '%s\n' "ExecStart=/home/nym/nym-mixnode_linux_x86_64 run --id SF" >> /etc/systemd/system/nym-mixnode.service
+     printf '%s\n' "ExecStart=/home/nym/nym-mixnode_linux_x86_64 run --id NymMixNode" >> /etc/systemd/system/nym-mixnode.service
      printf '%s\n' "KillSignal=SIGINT" >> /etc/systemd/system/nym-mixnode.service				
      printf '%s\n' "Restart=on-failure" >> /etc/systemd/system/nym-mixnode.service
      printf '%s\n' "RestartSec=30" >> /etc/systemd/system/nym-mixnode.service
@@ -76,7 +76,7 @@ function upgrade_nym () {
       printf "%b\n\n\n" "${WHITE} Printing of the systemd script to the current folder ${RED} failed. ${WHITE} Do you have ${YELLOW} permissions ${WHITE} to ${YELLOW} write ${WHITE} in ${pwd} ${YELLOW}  directory ??? "
     fi
     cd /home/nym/
-    sudo -u nym -H ./nym-mixnode_linux_x86_64 upgrade --id /home/nym/.nym/mixnodes/SF    
+    sudo -u nym -H ./nym-mixnode_linux_x86_64 upgrade --id /home/nym/.nym/mixnodes/NymMixNode    
 }
 #set -x
 downloader && echo "ok" && sleep 2 || exit 1
